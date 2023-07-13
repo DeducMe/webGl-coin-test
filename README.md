6c334a0bd8511dafbf75fa0ba2b4a9b9d1f6dca5
DeducMe <deducme@gmail.com>
2023-07-13 19:03:32 +0300

    readme + microfixes

---
+++ README.md
@@ -0,0 +1,1 @@
+Here I attempted to use Three.js with a coin model that i found online
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const path = require('path');
-const THREE = require("three")
-var http = require('http');
-var bodyParser = require('body-parser');
+const path = require("path");
+const THREE = require("three");
+var http = require("http");
+var bodyParser = require("body-parser");
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 3000);
 var server = http.createServer(app);
-app.use(express.static(path.join(__dirname, './front')));
+app.use(express.static(path.join(__dirname, "./front")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './front', 'index.html')))
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./front", "index.html"))
+);
 
-server.listen(process.env.PORT || 5000);
-
-
+server.listen(process.env.PORT || 3000);
